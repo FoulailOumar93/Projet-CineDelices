e6d24daa23864b5b9fe38a9541b443da023fc356
foulailoumar93 <foulailoumar55@gmail.com>
2026-05-24 19:15:53 +0200

    frontend production api

--- api/sequelize.client.js
+++ api/sequelize.client.js
@@ -1,12 +1,13 @@
 import { Sequelize } from 'sequelize';
 
-export const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    logging: false,
+export const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
   },
-);
+});
